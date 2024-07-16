27b465e038339c4b1337b2fe2dda97747b92b9f3
Myeong Soo Han <hms91018717@gmail.com>
2024-07-16 15:44:46 +0900

    Update graph_llm.py

--- src/model/graph_llm.py
+++ src/model/graph_llm.py
@@ -32,7 +32,8 @@ class GraphLLM(torch.nn.Module):
 
         print('Loading LLAMA')
         kwargs = {
-            "max_memory": {0: '80GiB', 1: '80GiB'},
+            "max_memory": {0: '80GiB'},
+            # "max_memory": {0: '80GiB', 1: '80GiB'},
             "device_map": "auto",
             "revision": "main",
         }
